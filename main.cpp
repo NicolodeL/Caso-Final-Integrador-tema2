b3ffdb3cf772fef619121ecc2f838b9ed37b7b85
NlodeL <nlodelop@myuax.com>
2023-11-30 11:59:59 +0100

    Hago multiples pruebas y acabo añadiendo un archivo txt como ejemplo

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
+#include <fstream>
+using namespace std;
+
+void load_script(const char* Ejemplo, bool show_script = false) {
+    std::ifstream file(Ejemplo);
+    };
+
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
